c0a4c0cef9c9f263ad5e33b818f516f1081908ba
Walaks Silva <wsilva@monumenta.com.br>
2019-06-11 19:44:42 -0300

    Aula#2

---
+++ Aula#2/backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socked.io')(server);
+
+mongoose.connect('mongodb+srv://curso:brasil2010@cluster0-sbkpe.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser : true,
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+})
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
+app.use(require('./routes'));
+
+app.listen(3333);
